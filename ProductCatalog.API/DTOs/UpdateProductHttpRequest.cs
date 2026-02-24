fdfc058cf436909b89a38576378386035e79d5f1
annafgomes <anna.gomes@dcomp.ufs.br>
2026-02-24 18:38:13 -0300

    feat(DTO): cria DTO UpdateProduct

---
+++ ProductCatalog.API/DTOs/UpdateProductHttpRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductCatalog.API.DTOs
+{
+    /// <summary>
+    /// DTO para atualizar um produto
+    /// </summary>
+    public class UpdateProductHttpRequest
+    {
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public string Category { get; set; } = default!;
+    }
+}

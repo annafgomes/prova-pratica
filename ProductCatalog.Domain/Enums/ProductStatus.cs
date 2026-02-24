73380ff7e57df1f27d03ce25814d21959daf1b11
annafgomes <anna.gomes@dcomp.ufs.br>
2026-02-24 11:05:42 -0300

    feat(domain): adiciona enum ProductStatus

---
+++ ProductCatalog.Domain/Enums/ProductStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductCatalog.Domain.Enums;
+
+/// <summary>
+/// Representa o status atual do produto  no catálogo .
+/// </summary>
+public enum ProductStatus
+{
+    Active = 1,
+    Inactive = 2
+}

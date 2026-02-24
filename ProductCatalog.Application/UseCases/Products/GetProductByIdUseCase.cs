ae5939d1a44ea7c1faedba3aaad04cf3cb46cf13
annafgomes <anna.gomes@dcomp.ufs.br>
2026-02-24 14:28:48 -0300

    feat(application): adiciona GetProductbyIdUseCase

---
+++ ProductCatalog.Application/UseCases/Products/GetProductByIdUseCase.cs
@@ -0,0 +1,30 @@
+﻿using ProductCatalog.Domain.Entities;
+using ProductCatalog.Domain.Interfaces;
+
+namespace ProductCatalog.Application.UseCases.Products;
+
+/// <summary>
+/// Caso de uso responsavel por buscar um produto pelo Id
+/// </summary>
+public class GetProductByIdUseCase
+{
+    private readonly IProductRepository _repository;
+
+    public GetProductByIdUseCase(IProductRepository repository)
+    {
+        _repository = repository;
+    }
+
+    /// <summary>
+    /// Executa a busca de produto pelo  Id
+    /// </summary>
+    public async Task<Product> ExecuteAsync(Guid id)
+    {
+        var product = await _repository.GetByIdAsync(id);
+
+        if (product is null)
+            throw new KeyNotFoundException("Product not found.");
+
+        return product;
+    }
+}

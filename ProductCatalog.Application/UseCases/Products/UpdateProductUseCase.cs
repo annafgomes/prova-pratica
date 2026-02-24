3058449091b5235f16e49e504a30e97d8fb4e346
annafgomes <anna.gomes@dcomp.ufs.br>
2026-02-24 14:57:08 -0300

    feat(application): adiciona UpdateProductUseCase

---
+++ ProductCatalog.Application/UseCases/Products/UpdateProductUseCase.cs
@@ -0,0 +1,42 @@
+﻿using ProductCatalog.Domain.Entities;
+using ProductCatalog.Domain.Interfaces;
+
+namespace ProductCatalog.Application.UseCases.Products;
+
+/// <summary> 
+/// Caso de uso responsavel  por atualizar um produto
+/// </summary>
+public class UpdateProductUseCase
+{
+    private readonly IProductRepository _repository;
+
+    public UpdateProductUseCase(IProductRepository repository)
+    {
+        _repository = repository;
+    }
+
+    /// <summary>
+    /// Executa a atualizacao d o produto
+    /// </summary>
+    public async Task ExecuteAsync(Guid id, string name, string description, decimal price, int stockQuantity, string category)
+    {
+        // Busca o produto no repositorio
+        var product = await _repository.GetByIdAsync(id);
+
+        // Verifica se o produto existe
+        if (product is null)
+            throw new KeyNotFoundException("Product not found.");
+
+        // Atualiza dados principais
+        product.UpdateDetails(name, description, category);
+
+        // Atualiza preco
+        product.UpdatePrice(price);
+
+        // Atualiza estoque
+        product.UpdateStock(stockQuantity);
+
+        // Salva a atualizacao
+        await _repository.UpdateAsync(product);
+    }
+}

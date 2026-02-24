2408c201d19c5458d1280ce1aa15f39abb0fbd54
annafgomes <anna.gomes@dcomp.ufs.br>
2026-02-24 12:52:13 -0300

    feat(application): adiciona CreateProductRequest

---
+++ ProductCatalog.Application/UseCases/Products/CreateProductRequest.cs
@@ -0,0 +1,33 @@
+﻿namespace ProductCatalog.Application.UseCases.Products;
+
+/// <summary>
+/// DTO responsável por transportar os dados necessários
+/// para criação de um produto
+/// </summary>
+public class CreateProductRequest
+{
+    /// <summary>
+    /// Nome do produto
+    /// </summary>
+    public string Name { get; set; } = default!;
+
+    /// <summary>
+    /// Descrição do produto.
+    /// </summary>
+    public string Description { get; set; } = default!;
+
+    /// <summary>
+    /// Preço de venda
+    /// </summary>
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// Quantidade em estoque
+    /// </summary>
+    public int StockQuantity { get; set; }
+
+    /// <summary>
+    /// Categoria do produto.
+    /// </summary>
+    public string Category { get; set; } = default!;
+}

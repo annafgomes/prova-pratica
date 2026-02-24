7398956d1239844723211ba66521e5208bfdda1f
annafgomes <anna.gomes@dcomp.ufs.br>
2026-02-24 12:54:58 -0300

    feat(application): adiciona CreateProductUseCase

---
+++ ProductCatalog.Application/UseCases/Products/CreateProductUseCase.cs
@@ -0,0 +1,36 @@
+﻿using ProductCatalog.Domain.Entities;
+using ProductCatalog.Domain.Interfaces;
+
+namespace ProductCatalog.Application.UseCases.Products;
+
+/// <summary>
+/// Caso de uso responsável por criar um novo  produto
+/// </summary>
+public class CreateProductUseCase
+{
+    private readonly IProductRepository _repository;
+
+    public CreateProductUseCase(IProductRepository repository)
+    {
+        _repository = repository;
+    }
+
+
+
+    /// <summary>
+    /// Executa a criação de um novo produto
+    /// </summary>
+    public async Task<Guid> ExecuteAsync(CreateProductRequest request)
+    {
+        var product = new Product(
+            request.Name,
+            request.Description,
+            request.Price,
+            request.StockQuantity,
+            request.Category);
+
+        await _repository.AddAsync(product);
+
+        return product.Id;
+    }
+}

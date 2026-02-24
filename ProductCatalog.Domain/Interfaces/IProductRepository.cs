6e6769914fe0a54f98911ccbfb5212465bd081f3
annafgomes <anna.gomes@dcomp.ufs.br>
2026-02-24 12:46:07 -0300

    feat(interfaces): adiciona interface IProductRepository na camada Domain

---
+++ ProductCatalog.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProductCatalog.Domain.Entities;
+
+namespace ProductCatalog.Domain.Interfaces;
+
+/// <summary>
+/// Define o contrato de persistência para a entidade Product.
+/// </summary>
+public interface IProductRepository
+{
+    Task<Product?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task AddAsync(Product product);
+    Task UpdateAsync(Product product);
+    Task DeleteAsync(Product product);
+}

0ac2082cfc5e8aa06a7a0abf768fd8b24b4d772e
annafgomes <anna.gomes@dcomp.ufs.br>
2026-02-24 14:13:30 -0300

    feat(application): adiciona GetAllProductsUseCase

---
+++ ProductCatalog.Application/UseCases/Products/GetAllProductsUseCase.cs
@@ -0,0 +1,25 @@
+﻿using ProductCatalog.Domain.Entities;
+using ProductCatalog.Domain.Interfaces;
+
+namespace ProductCatalog.Application.UseCases.Products;
+
+/// <summary>
+/// Caso de uso responsável por listar todos os produtos.
+/// </summary>
+public class GetAllProductsUseCase
+{
+    private readonly IProductRepository _repository;
+
+    public GetAllProductsUseCase(IProductRepository repository)
+    {
+        _repository = repository;
+    }
+
+    /// <summary>
+    /// Executa a listagem de produtos.
+    /// </summary>
+    public async Task<IEnumerable<Product>> ExecuteAsync()
+    {
+        return await _repository.GetAllAsync();
+    }
+}
